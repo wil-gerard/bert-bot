e2fe4d3a5bd4491f00792d56ec608ce40c7395ea
wil-gerard <williamrgerard@gmail.com>
2021-08-23 14:09:46 -0500

    Got a submit button working

--- src/App.js
+++ src/App.js
@@ -1,38 +1,46 @@
-import React, { useRef, useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-import * as tf from "@tensorflow/tfjs";
-import * as qna from "@tensorflow-models/qna";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Loader from "react-loader-spinner";
-import {Fragment} from "react";
+import * as tf from "@tensorflow/tfjs"
+import * as qna from "@tensorflow-models/qna"
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from "react-loader-spinner"
+import {Fragment} from "react"
 
 const App = () => {
 
-  const passageRef = useRef(null);
+  const passageRef = useRef(null)
   const questionRef = useRef(null);
-  const [answer, setAnswer] = useState();
-  const [model, setModel] = useState(null);
+  const [answer, setAnswer] = useState()
+  const [model, setModel] = useState(null)
 
   // Async function loading the tensorflow Q&A model
   const loadModel = async () => {
     const loadedModel = await qna.load()
-    setModel(loadedModel);
+    setModel(loadedModel)
     console.log('Model loaded')
   }
 
   // Runs the async function for loading the model with an empty array after to stop it from reloading during rerendering
   useEffect(() => { loadModel() }, [])
 
-  const answerQuestion = async (e) => {
-    if (e.which === 13 && model !== null) { // If the event of enter button AKA "13" is pressed and the model has loaded..
+  // e.which === 13 // does the event handler (e) have a key code property (which) of 13?
+
+  // const handleKeyPress = e => {
+  //   if (e.keyCode === 13) {
+  //     this.btn.click()
+  //   }
+  // } 
+
+  const answerQuestion = async () => {
+    if (model !== null) { // If the event of enter button AKA "13" is pressed and the model has loaded..
       console.log('Question has been submitted') 
       const passage = passageRef.current.value 
       const question = questionRef.current.value
 
       const answers = await model.findAnswers(question, passage) // Passing the passage and question as references to the Q&A model
-      setAnswer(answers) // Takes the answers and pushes the information to the answer state
+      setAnswer(answers) // Takes the answers and pushes the information to the answer state. Console logging the results
       console.log(answers)
     }
   }
@@ -44,7 +52,7 @@ const App = () => {
       { model == null ? 
       // Ternary operator checking if the model is null -> if model is null, loader spinner function appears
         <div> 
-          <div>Machine Learning Model is Loading..</div>
+          <div>BERT is Loading...</div>
           <Loader
             type="Puff"
             color="#00BFFF"
@@ -57,7 +65,8 @@ const App = () => {
           Passage
           <textarea ref={passageRef} rows="30" cols="100"></textarea>
           Ask a Question
-          <input ref={questionRef} onKeyPress={answerQuestion} size="80"></input>
+          <input ref={questionRef} size="80"></input>
+          <button onClick={answerQuestion} type="submit">submit</button>
           Answers
           {answer ? answer.map((answer, index) => <div> <b> Answer {index + 1} - </b> {answer.text} ({ Math.floor(answer.score * 100) / 100 }) </div>) : ""}
         </Fragment> 
@@ -65,7 +74,7 @@ const App = () => {
     
         </header>
     </div>
-  );
+  )
 }
 
 export default App
