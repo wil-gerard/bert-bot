36f8a4004245aff90f978d49b3bc66ba315f3819
wil-gerard <williamrgerard@gmail.com>
2021-09-03 15:03:57 -0500

    added default values for the form and input

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import * as tf from "@tensorflow/tfjs"
 import * as qna from "@tensorflow-models/qna"
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import Loader from "react-loader-spinner"
-import {Fragment} from "react"
 
 const App = () => {
 
@@ -61,15 +60,19 @@ const App = () => {
         </div>
         :
         // -> if model is not null (has loaded) fragment function runs to define UI
-        <Fragment>
-          Passage
-          <textarea ref={passageRef} rows="30" cols="100"></textarea>
-          Ask a Question
-          <input ref={questionRef} size="80"></input>
+        <>
+        <h1>{`BERT Q&A`}</h1>
+          <textarea ref={passageRef} rows="30" cols="100" defaultValue={`The discography of Black Sabbath, an English heavy metal band, includes 19 studio albums, eight live albums, 16 compilation albums, seven video albums, one extended play and 30 singles. The band was formed in 1968 by John "Ozzy" Osbourne (vocals), Tony Iommi (lead guitar), Terence "Geezer" Butler (bass guitar), and Bill Ward (drums). Throughout their history, the band experienced multiple lineup changes.[1] Though the second most recent line-up of the band to work together were Ronnie James Dio, Vinny Appice, Iommi, and Butler, for three new songs for a compilation in 2007, the original line-up was still considered the "current" lineup at the time and had been since their reunion in 1997. The 2007 sessions were deemed a one-off which led to the Heaven and Hell side project, resulting in a new studio album in 2009 titled The Devil You Know. In June 2013, a partial reformation of the original line-up released 13, which was the first album to feature Osbourne on vocals since 1978's Never Say Die!. After 49 years together, Black Sabbath announced their breakup in March 2017.`}></textarea>
+          <div>Ask a Question</div>
+          <input ref={questionRef} size="80" type="text" placeholder="Ask BERT a question" defaultValue="When was Black Sabbath formed?"></input>
           <button onClick={answerQuestion} type="submit">submit</button>
-          Answers
-          {answer ? answer.map((answer, index) => <div> <b> Answer {index + 1} - </b> {answer.text} ({ Math.floor(answer.score * 100) / 100 }) </div>) : ""}
-        </Fragment> 
+          <div>
+            <div>Answers</div>
+            <div>
+              {answer ? answer.map((answer, index) => <div> <b> Answer {index + 1} - </b> {answer.text} ({ Math.floor(answer.score * 100) / 100 }) </div>) : ""}
+            </div>
+          </div> 
+        </> 
     }
     
         </header>
