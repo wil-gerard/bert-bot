a7b960744a832ad27a0eb0a770f4df12e9cb56b9
wil-gerard <williamrgerard@gmail.com>
2021-09-08 16:37:04 -0500

    added material ui typography stylings

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import './App.css'
 import * as tf from "@tensorflow/tfjs"
 import * as qna from "@tensorflow-models/qna"
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
-import { TextField, Button, Grid } from "@material-ui/core"
+import { TextField, Button, Typography } from "@material-ui/core"
 import Loader from "react-loader-spinner"
-import { sizing } from '@material-ui/system';
 
 const App = () => {
 
@@ -57,18 +56,18 @@ const App = () => {
         {model == null ?
           // Ternary operator checking if the model is null -> if model is null, loader spinner function appears
           <div>
-            <div className="loader-text">BERT model is loading...</div>
+            <Typography variant="h3" style={{ fontWeight: 600}}>loading BERT model...</Typography> 
             <Loader
               type="Puff"
               color="#2886E7"
-              height={120}
-              width={120}
+              height={200}
+              width={200}
             />
           </div>
           :
           // -> if model is not null (has loaded) fragment function runs to define UI
           <>
-            <h1>{`BERT Q&A`}</h1>
+            <Typography variant="h2" style={{ fontWeight: 600}}>{`BERT Q&A`}</Typography>
                 <TextField
                   label="Text passage"
                   variant="outlined"
@@ -79,22 +78,24 @@ const App = () => {
                   defaultValue={`The discography of Black Sabbath, an English heavy metal band, includes 19 studio albums, eight live albums, 16 compilation albums, seven video albums, one extended play and 30 singles. The band was formed in 1968 by John "Ozzy" Osbourne (vocals), Tony Iommi (lead guitar), Terence "Geezer" Butler (bass guitar), and Bill Ward (drums). Throughout their history, the band experienced multiple lineup changes.[1] Though the second most recent line-up of the band to work together were Ronnie James Dio, Vinny Appice, Iommi, and Butler, for three new songs for a compilation in 2007, the original line-up was still considered the "current" lineup at the time and had been since their reunion in 1997. The 2007 sessions were deemed a one-off which led to the Heaven and Hell side project, resulting in a new studio album in 2009 titled The Devil You Know. In June 2013, a partial reformation of the original line-up released 13, which was the first album to feature Osbourne on vocals since 1978's Never Say Die!. After 49 years together, Black Sabbath announced their breakup in March 2017.`}
                 />
 
-
+                <div className="question">
                 <TextField
                   margin="dense"
                   variant="outlined"
                   label="Ask a Question"
                   inputRef={questionRef}
-                  style= {{width: 400}}
+                  style= {{width: 600}}
                   type="text"
                   defaultValue="When was Black Sabbath formed?"
                 />
+<Button style={{margin: "8px 0 4px 6px"}} variant="outlined" onClick={answerQuestion} type="submit">submit</Button>
+</div>
 
-              <Button variant="contained" onClick={answerQuestion} type="submit">submit</Button>
+              
             <div className="answer-container">
-              <h2>Answers</h2>
+              <Typography variant="h4" style={{ fontWeight: 600}}>Answers</Typography>
               <div>
-                {answer ? answer.map((answer) => <div className="answers"> <b> Confidence level: {Math.floor(answer.score * 100) / 100} - </b> {answer.text}   </div>) : ""}
+                {answer ? answer.map((answer, index) => <Typography align="left"> <b>{ index + 1 }.</b> {answer.text}  (Confidence level: {Math.floor(answer.score * 100) / 100}) </Typography>) : ""}
               </div>
             </div>
           </>
